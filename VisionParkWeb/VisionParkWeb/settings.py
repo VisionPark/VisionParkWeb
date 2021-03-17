dac59e79b85e9d0a8840699c1bdd38ff9423d2ca
Narovik <victornarov@gmail.com>
2021-03-17 12:06:00 +0100

    Secret Key

--- VisionParkWeb/VisionParkWeb/settings.py
+++ VisionParkWeb/VisionParkWeb/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import secret_settings
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '%a(p3osw1gm#egdzfvts(^5p6hnd+hm+x#4h^nwtbs$jzs*4lb'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
